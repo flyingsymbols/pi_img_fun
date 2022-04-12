ecf0b1744afe7bd0229d4267b47e7deeb95da07f
David Hogarty <david.hogarty@gmail.com>
2022-04-11 21:34:55 -0400

    added a bpp without truncation to see how fast it converges

--- cf.py
+++ cf.py
@@ -4,71 +4,43 @@ import sympy
 from sympy import Abs, pi, floor
 import sympy.ntheory.continued_fraction as cf_mod
 
-def main():
-    N=200
-    pi_cf_list = list(pi_recurrance_1(N))
-    prev_pi_cf = 0
-    for i in range(0, N):
-        cur_pi_cf = pi_cf_n(i)
-        diff = Abs(cur_pi_cf - prev_pi_cf)
-        diff_ln = -sympy.ln(diff).evalf()
-        diff_log = -sympy.ln(diff, 10).evalf()
-        print(f"{i}: {cur_pi_cf}, {pi_cf_list[i]}, e:{diff_ln:.2f}, 10:{diff_log:.2f}")
+S = sympy.sympify
 
-        prev_pi_cf = cur_pi_cf
+def main():
+    N=50
+    # pi_list = list(pi_recurrance_1(N))
+    # pi_list = list(pi_recurrance_2(N))
+    pi_list = list(pi_bpp(N))
+    prev_pi = 0
+    for i in range(0, N):
+        cur_pi = pi_list[i]
+        diff = Abs(cur_pi - prev_pi)
+        diff_ln = float(-sympy.ln(diff).evalf())
+        diff_log = float(-sympy.ln(diff, 10).evalf())
+        print(f"{i}: {cur_pi}, e:{diff_ln:.2f}, 10:{diff_log:.2f}")
+
+        prev_pi = cur_pi
 
 class RequirementError(RuntimeError): pass
 def require(cond, msg):
     if not cond:
         raise RequirementError(msg)
 
-def pi_cf_n(n):
-    """
-    Uses the definition of pi as
+bpp_numerators = (4, -2, -1, -1)
+bpp_offsets = (1, 4, 5, 6)
 
-        inf
-    3 + K   (2x-1)**2/6
-        x=1
+def pi_bpp(n):
+    total = 0
+    for k in range(n):
+        denom_base = 8*k
+        val = 0
+        for i in range(4):
+            val += S(bpp_numerators[i])/S(denom_base + bpp_offsets[i])
 
-    but generates and returns the values where instead of an infinite
-    continued fraction, it is a finite continued fraction with n terms
-    """
-    require(n >= 0, f'failed: n:{n} >= 0')
-    
-    sub_frac = 0
-    for x in range(n,0,-1):
-        denom = 6 + sub_frac
+        val *= 1/S(16**k)
 
-        numer = (2*x - 1)**2
-        sub_frac = numer/denom
-
-    return 3 + sub_frac
-
-def pi_cf_n_recurrance(n):
-    """
-    Uses the definition of pi as
-
-        inf
-    3 + K   (2x-1)**2/6
-        x=1
-
-    and the general continued fraction recurrance
-    """
-    A_prev = 1
-    B_prev = 0
-    B_cur = 1
-    A_cur = 3
-    yield A_cur/B_cur
-    for i in range(1, n):
-        a_next = (2*i-1)**2
-        b_next = 6
-        A_next = b_next*A_cur+a_next*A_prev
-        B_next = b_next*B_cur+a_next*B_prev
-        A_prev = A_cur
-        B_prev = B_cur
-        A_cur = A_next
-        B_cur = B_next
-        yield A_cur/B_cur
+        total += val
+        yield total
 
 def pi_recurrance_1(n):
     """
@@ -88,7 +60,28 @@ def pi_recurrance_1(n):
     den_iter = itertools.repeat(6)
 
     yield from cf_n_recurrance(3, num_iter, den_iter, n)
-    
+
+def pi_recurrance_2(n):
+    """
+    Uses the definition of pi/2 as
+
+    successive an/bn for general continued recurrance
+
+    an   -1 -2*3 -1*2 -4*5 -3*4 -6*7 -5*6
+
+    bn    3    1    3    1    3    1    3
+    """
+
+    def num_iter_func():
+        yield -1
+        for i in itertools.count(start=2, step=2):
+            yield -i*(i+1)
+            yield -(i-1)*i
+
+    num_iter = iter(num_iter_func())
+    den_iter = itertools.cycle([3,1])
+
+    yield from cf_n_recurrance(1, num_iter, den_iter, 200)
 
 def cf_n_recurrance(start_val, a_iter, b_iter, n):
     A_prev = 1
@@ -106,17 +99,6 @@ def cf_n_recurrance(start_val, a_iter, b_iter, n):
         A_cur = A_next
         B_cur = B_next
         yield A_cur/B_cur
-
-def pi_cf_n_recurrance_2(n):
-    """
-    Uses the definition of pi/2 as
-
-    successive an/bn for general continued recurrance
-
-    an   1 -2*3 -1*2 -4*5 -3*4
-
-    bn   3    1    3    1    3
-    """
 
 if __name__ == '__main__':
     main()
